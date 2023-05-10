b3e9f7aa1124c23db6ac0179eeac5e4de3864df7
haze998 <docenko6996@gmail.com>
2023-05-11 01:04:52 +0300

    initial-commit

---
+++ BetMate/BetMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BetMate
+//
+//  Created by Evgeniy Docenko on 11.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
