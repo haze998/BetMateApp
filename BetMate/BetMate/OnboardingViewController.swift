7abab13065b56f231b64a0c4f7b30115aa02ec4f
haze998 <docenko6996@gmail.com>
2023-05-11 01:25:16 +0300

    add-fonts-add-snapkit-delete-main-storyboard

---
+++ BetMate/BetMate/OnboardingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  OnboardingViewController.swift
+//  BetMate
+//
+//  Created by Evgeniy Docenko on 11.05.2023.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        
+    }
+
+    // MARK: - Setup UI
+    private func setupUI() {
+        self.view.backgroundColor = .cyan
+    }
+
+}
+
